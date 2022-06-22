bf3ab84f47e9ecf740e9fc47d16e1196321ec09e
Masterpluis <jurriaan-flikkema@hotmail.com>
2022-06-23 01:40:36 +0200

    First commit

---
+++ src/script.ts
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  console.log("JavaScript is linked.");
+
+  const list = document.getElementById("list") as HTMLUListElement;
+  const form = document.getElementById("addTodoItem") as HTMLFormElement;
+  const input = document.getElementById("newTask") as HTMLInputElement;
+});
