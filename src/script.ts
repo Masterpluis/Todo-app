f077d061adf8b7574657b1118b9ccb716ad74d39
Masterpluis <jurriaan-flikkema@hotmail.com>
2022-06-27 00:05:02 +0200

    LocalStorage added

--- src/script.ts
+++ src/script.ts
@@ -1,95 +1,94 @@
 window.addEventListener("load", () => {
-  enum TaskStatus {
-    default,
-    inProgress,
-    completed,
-  }
+	enum TaskStatus {
+		default,
+		inProgress,
+		completed,
+	}
 
-  class TodoApp {
-    private list = document.getElementById("list") as HTMLUListElement;
-    private form = document.getElementById("addTodoItem") as HTMLFormElement;
-    private input = document.getElementById("newTask") as HTMLInputElement;
-    public tasks: Task[] = [];
-    constructor() {
-      this.form?.addEventListener("submit", (e) => this.addTask(e));
-    }
+	type TaskData = {
+		status: TaskStatus;
+		title: string;
+	};
 
-    private addTask(e: SubmitEvent) {
-      e.preventDefault();
-      if (this.input?.value == "" || this.input?.value == null) return;
+	class TodoApp {
+		public list = document.getElementById("list") as HTMLUListElement;
+		private form = document.getElementById("addTodoItem") as HTMLFormElement;
+		private inputTitle = document.getElementById("newTask") as HTMLInputElement;
+		public tasks: TaskData[] = this.loadTasks();
+		constructor() {
+			this.tasks.forEach((taskData) => new Task(this, taskData));
+			this.form?.addEventListener("submit", (e) => {
+				e.preventDefault();
+				if (this.inputTitle?.value == "" || this.inputTitle?.value == null) return;
 
-      this.tasks.push(new Task(this, this.list, this.input.value));
-      this.input.value = "";
-      this.updateList();
-    }
+				this.tasks.push(new Task(this, {status: 0, title: this.inputTitle.value}).data);
+				this.saveTasks();
+				this.inputTitle.value = "";
+			});
+		}
 
-    public removeTask(task: Task) {
-      this.tasks.splice(this.tasks.indexOf(task), 1);
-      this.updateList();
-    }
+		private loadTasks() {
+			const tasksJSON = localStorage.getItem("TASKS");
+			if (tasksJSON == null) return [];
+			return JSON.parse(tasksJSON);
+		}
 
-    public updateList() {}
-  }
+		public saveTasks() {
+			const taskJSON = JSON.stringify(this.tasks);
+			localStorage.setItem("TASKS", taskJSON);
+		}
+	}
 
-  class Task {
-    public root: HTMLLIElement;
-    private _status: TaskStatus = 0;
+	class Task {
+		private root: HTMLLIElement;
+		constructor(private owner: TodoApp, public data: TaskData) {
+			this.root = document.createElement("li");
+			const label = document.createElement("label");
+			const checkbox = document.createElement("input");
+			checkbox.type = "checkbox";
+			label.append(checkbox, this.data.title);
+			const rIcon = document.createElement("img");
+			rIcon.src = "../media/trashIcon.svg";
+			const rBtn = document.createElement("a");
+			rBtn.append(rIcon);
+			this.root.append(label, rBtn);
+			this.owner.list.prepend(this.root);
 
-    public set status(status: TaskStatus) {
-      this._status = status;
-    }
+			this.setStatus();
+			checkbox.addEventListener("change", () => this.changeStatus());
+			rBtn.addEventListener("click", () => this.removeTask());
+		}
 
-    public get status() {
-      return this._status;
-    }
+		private setStatus() {
+			if (this.data.status == 1) this.root.classList.add("inProgress");
+			if (this.data.status == 2) this.root.classList.add("completed");
+		}
 
-    constructor(
-      private owner: TodoApp,
-      private parent: HTMLUListElement,
-      private title: string
-    ) {
-      this.root = document.createElement("li");
-      const label = document.createElement("label");
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      label.append(checkbox, this.title);
+		private changeStatus() {
+			switch (this.data.status) {
+				case 0:
+					this.data.status = 1;
+					this.root.classList.add("inProgress");
+					break;
+				case 1:
+					this.data.status = 2;
+					this.root.classList.remove("inProgress");
+					this.root.classList.add("completed");
+					break;
+				case 2:
+					this.data.status = 0;
+					this.root.classList.remove("completed");
+					break;
+			}
+			this.owner.saveTasks();
+		}
+		
+		private removeTask() {
+			this.root.remove();
+			this.owner.tasks.splice(this.owner.tasks.indexOf(this.data), 1);
+			this.owner.saveTasks();
+		}
+	}
 
-      const rIcon = document.createElement("img");
-      rIcon.src = "../media/trashIcon.svg";
-      const rBtn = document.createElement("a");
-      rBtn.append(rIcon);
-
-      this.root.append(label, rBtn);
-      this.parent.prepend(this.root);
-
-      checkbox.addEventListener("change", () => this.changeStatus());
-      rBtn.addEventListener("click", () => this.remove());
-    }
-
-    private changeStatus() {
-      switch (this.status) {
-        case 0:
-          this.status = 1;
-          this.root.classList.add("inProgress");
-          break;
-        case 1:
-          this.status = 2;
-          this.root.classList.remove("inProgress");
-          this.root.classList.add("completed");
-          break;
-        case 2:
-          this.status = 0;
-          this.root.classList.remove("completed");
-          break;
-      }
-      this.owner.updateList();
-    }
-
-    private remove() {
-      this.root.remove();
-      this.owner.removeTask(this);
-    }
-  }
-
-  new TodoApp();
+	new TodoApp();
 });
